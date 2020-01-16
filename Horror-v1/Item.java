56c3d0fc5303581b799f0464daf297e3114fe676
quinten138 <quinten.s.ibrahim@gmail.com>
2020-01-16 13:02:28 +0100

    naamgoed

---
+++ Horror-v1/Item.java
@@ -0,0 +1,20 @@
+
+/**
+ * Write a description of class Item here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class Item
+{
+    String description;
+    
+    public Item(String newdescription)
+    {
+        description = newdescription;
+    }
+    public String getDescription()
+    {
+        return description;
+    }
+}
